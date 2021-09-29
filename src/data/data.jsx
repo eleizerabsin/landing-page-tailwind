1b3384f5728d9fd7c3d908f94d79dcf7268f5cee
eabsin <eleizer.absin@gmail.com>
2021-09-29 11:39:25 +0800

    simplify code

---
+++ src/data/data.jsx
@@ -0,0 +1,69 @@
+import introImage from "../images/img1.svg";
+import aboutImage from "../images/img2.svg";
+import reactIcon from "../images/react-icon.svg";
+
+export const intro = {
+  smallHeader: "Duis iaculis",
+  header: "Fusce dapibus ultrices ipsum vitae blandit.",
+  description:
+    "Morbi sit amet tempor tortor, at congue ipsum. Praesent sit amet nisi commodo, iaculis nibh non, iaculis tellus. Vestibulum at suscipit risus, vel efficitur elit. Pellentesque felis lectus, placerat eget efficitur at, tincidunt eu magna.",
+  phone: "P: +123 456 7890",
+  email: "E: Morbisuscipit@mail.com",
+  button: "Praesent sit",
+  image: introImage,
+};
+
+export const about1 = {
+  smallHeader: "Duis iaculis",
+  header: "Fusce dapibus ultrices ipsum vitae blandit.",
+  description:
+    "Morbi sit amet tempor tortor, at congue ipsum. Praesent sit amet nisi commodo, iaculis nibh non, iaculis tellus. Vestibulum at suscipit risus, vel efficitur elit. Pellentesque felis lectus, placerat eget efficitur at, tincidunt eu magna.",
+  numberHighlightNo: "4",
+  numberHighlightText: "Donec Fermentum",
+};
+
+export const about2 = {
+  header: "Praesent iaculis sed nunc.",
+  bigHeader: true,
+  description:
+    "Mauris pulvinar eget erat eget aliquam. Cras pulvinar tellus in nisl fermentum euismod",
+  bigDescription: true,
+  link: "rutrum-elementum@email.com",
+};
+
+export const about3 = {
+  smallHeader: "Duis iaculis",
+  header: "Fusce dapibus ultrices ipsum vitae blandit.",
+  description:
+    "Morbi sit amet tempor tortor, at congue ipsum. Praesent sit amet nisi commodo, iaculis nibh non, iaculis tellus. Vestibulum at suscipit risus, vel efficitur elit. Pellentesque felis lectus, placerat eget efficitur at, tincidunt eu magna.",
+  numberHighlightNo: "4",
+  numberHighlightText: "Donec Fermentum",
+  image: aboutImage,
+};
+
+export const iconGrid = [
+  {
+    image: reactIcon,
+    text: "React",
+  },
+  {
+    image: reactIcon,
+    text: "React",
+  },
+  {
+    image: reactIcon,
+    text: "React",
+  },
+  {
+    image: reactIcon,
+    text: "React",
+  },
+  {
+    image: reactIcon,
+    text: "React",
+  },
+  {
+    image: reactIcon,
+    text: "React",
+  },
+];
