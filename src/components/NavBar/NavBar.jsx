9f518e117ff63333335385ff50f8d44894d8cd1b
eabsin <eleizer.absin@gmail.com>
2021-10-01 20:28:47 +0800

    updated text and fix warnings

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,18 +5,18 @@ const NavBar = () => {
   return (
     <div className="bg-bggray1">
       <div className="common-container">
-        <div class="nav-footer-grid">
+        <div className="nav-footer-grid">
           <div className="logo">el</div>
           <div className="mx-auto "></div>
           <div className="space-x-4 text-right text-2xl">
             <div className="inline-block">
-              <a href={github} target="_blank">
-                <i class="devicon-github-original"></i>
+              <a href={github} target="_blank" rel="noopener noreferrer">
+                <i className="devicon-github-original"></i>
               </a>
             </div>
             <div className="inline-block">
-              <a href={linkedin} target="_blank">
-                <i class="devicon-linkedin-plain"></i>
+              <a href={linkedin} target="_blank" rel="noopener noreferrer">
+                <i className="devicon-linkedin-plain"></i>
               </a>
             </div>
           </div>
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -92,9 +92,9 @@ export const SectionTypeFive = (props) => {
 
 const OneBlockGridImage = (props) => {
   var iconGrid = props.iconGrid
-    ? props.iconGrid.map((icon) => {
+    ? props.iconGrid.map((icon, index) => {
         return (
-          <div>
+          <div key={index}>
             <div className="image-capsule">
               <img alt="test" src={icon.image} />
             </div>
@@ -112,7 +112,7 @@ const OneBlockGridImage = (props) => {
         <SmallHeader text={props.iconGridHeader} />
       </div>
       <div className="text-center">
-        <div class="grid grid-cols-3 gap-4 justify-center">{iconGrid}</div>
+        <div className="grid grid-cols-3 gap-4 justify-center">{iconGrid}</div>
       </div>
     </div>
   );
@@ -147,6 +147,22 @@ const TwoBlockHeaderDesc = (props) => {
         ""
       )} */}
       {props.link ? <LinkText text={props.link} email={props.link} /> : ""}
+      {props.github && props.linkedin ? (
+        <div className="space-x-4 text-4xl text-lightblue my-4">
+          <div className="inline-block">
+            <a href={props.github} target="_blank" rel="noopener noreferrer">
+              <i className="devicon-github-original hover:text-white"></i>
+            </a>
+          </div>
+          <div className="inline-block">
+            <a href={props.linkedin} target="_blank" rel="noopener noreferrer">
+              <i className="devicon-linkedin-plain hover:text-white"></i>
+            </a>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
       {props.phone ? <SmallHeader text={props.phone} /> : ""}
       {props.email ? <SmallHeader text={props.email} /> : ""}
       {props.button ? <Button text={props.button} email={props.email} /> : ""}
@@ -163,12 +179,11 @@ const TwoBlockHeaderDesc = (props) => {
 };
 
 const ProjectList = (props) => {
-  console.log("props", props);
   var projectList =
     props.projectList && props.projectList.length > 0
-      ? props.projectList.map((item) => {
+      ? props.projectList.map((item, index) => {
           return (
-            <div className="py-4">
+            <div key={index} className="py-4">
               <p className="inline-block font-semibold text-lg  text-gray-400">
                 {item.header1}
               </p>
@@ -181,9 +196,14 @@ const ProjectList = (props) => {
               {item.listText && item.listText.length > 0 ? (
                 <ul className="list-inside">
                   {" "}
-                  {item.listText.map((listItem) => {
+                  {item.listText.map((listItem, listIndex) => {
                     return (
-                      <li className="my-2 text-sm text-gray-400">{listItem}</li>
+                      <li
+                        key={listIndex}
+                        className="my-2 text-sm text-gray-400"
+                      >
+                        {listItem}
+                      </li>
                     );
                   })}
                 </ul>
@@ -201,14 +221,14 @@ const ProjectList = (props) => {
 const HeaderText = (props) => {
   return props.bigHeader ? (
     props.typeWriterEffect ? (
-      <p className="font-semibold my-10 text-6xl text-transparent bg-clip-text bg-gradient-to-br from-gray-300 to-yellow-600">
+      <div className="font-semibold my-10 text-6xl text-transparent bg-clip-text bg-gradient-to-br from-gray-300 to-yellow-600">
         <TypeWriterEffect
           startDelay={20}
           cursorColor="gray"
           text={props.text}
           typeSpeed={100}
         />
-      </p>
+      </div>
     ) : (
       <p className="font-semibold my-10 text-6xl text-transparent bg-clip-text bg-gradient-to-br from-gray-300 to-yellow-600">
         {props.text}
@@ -233,7 +253,11 @@ const DescText = (props) => {
       <p className="my-4 text-base text-justify text-gray-400">{props.text}</p>
       {props.descImage ? (
         <div className="py-8">
-          <img className="rounded-3xl w-6/12" src={props.descImage}></img>
+          <img
+            className="rounded-3xl w-6/12"
+            src={props.descImage}
+            alt="desc-img"
+          ></img>
         </div>
       ) : (
         ""
@@ -256,7 +280,7 @@ const ImageOnly = (props) => {
   return (
     <div className="mx-auto p-12">
       <img
-        alt="img"
+        alt="section-img"
         className={props.imageRounded ? "rounded-3xl" : ""}
         src={props.image}
       />
@@ -282,9 +306,12 @@ const LinkText = (props) => {
     <a
       href={"mailto:" + props.email}
       target="_blank"
+      rel="noopener noreferrer"
       className="font-semibold my-4 text-2xl text-lightblue hover:underline"
     >
-      <div className="inline-block hover:underline">{props.text}</div>
+      <div className="inline-block hover:underline hover:text-white">
+        {props.text}
+      </div>
       <div className="inline-block mx-2">
         <svg
           xmlns="http://www.w3.org/2000/svg"
